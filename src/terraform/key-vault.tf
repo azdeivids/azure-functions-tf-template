61282c4121b7f05ffc3aad9dd5a7372049f41990
azdeivids <deivids.egle@gmail.com>
2023-10-13 15:43:41 +0100

    Permissions assigned

--- src/terraform/key-vault.tf
+++ src/terraform/key-vault.tf
@@ -13,7 +13,7 @@ resource "azurerm_key_vault" "main" {
 resource "azurerm_key_vault_access_policy" "terraform-user" {
   key_vault_id = azurerm_key_vault.main.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_user_assigned_identity.functions.principal_id
+  object_id    = data.azurerm_client_config.current.object_id
   depends_on   = [ azurerm_key_vault.main ]
 
   secret_permissions = [
