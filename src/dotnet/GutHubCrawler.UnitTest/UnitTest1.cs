6d3213440a3836b7ba3efc4c8616be4258f6e01c
Deivids <deivids.egle@gmail.com>
2023-10-13 19:12:49 +0100

    Rearranged solutions

--- src/dotnet/GutHubCrawler.UnitTest/UnitTest1.cs
+++ src/dotnet/GutHubCrawler.UnitTest/UnitTest1.cs
@@ -2,10 +2,11 @@ namespace GutHubCrawler.UnitTest
 {
     public class UnitTest1
     {
+        [Trait("Category", "Unit")]
         [Fact]
         public void Test1()
         {
-
+            Assert.True(true);
         }
     }
 }
